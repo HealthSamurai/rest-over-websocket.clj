eaf1f9b66d88b29e3bcb4635ec1b3a47988653fd
niquola <niquola@gmail.com>
2017-12-28 14:14:53 +0300

    implement rooms

---
+++ README.md
@@ -0,0 +1,15 @@
+## REST over WebSocket
+
+
+
+
+
+Connect to server by ws://host/$conn
+
+
+1. Serialization? json? edn? transit? or all of them?
+2. Request - Response by ring convetion 
+
+
+
+
--- backend/src/app/core.clj
+++ backend/src/app/core.clj
@@ -9,20 +9,34 @@
   {:status 200
    :body "hello"})
 
+(defn get-rooms [_]
+  {:body [{:id "clojure" :title "Clojure"}
+          {:id "haskel" :title "Haskel"}]})
+
 (def routes
   {:GET          {:action :index}
    "$conn"       {:GET {:action :connection}}
-   "admin"       {"users" {:GET {:action :get-users}}}})
+   "register" {:POST {:action :register}}
+   "rooms"    {:GET  {:action :rooms}
+               [:id] {:GET {:action :room}
+
+                      :POST {:action :add-message}}}})
+(defn index [_] {:body "<html></html>"})
+
+(def fns-map {:index index
+              :rooms #'get-rooms
+              :connection #'connection-handler})
 
 (defonce connections (atom #{}))
 
+(declare *dispatch)
+
 (defn dispatch-socket-request [req]
-  {:body {:some "data"
-          :request req}})
+  (println "dispatch" req)
+  (*dispatch req))
 
 (defn connection-handler [req]
   (server/with-channel req ch
-    (println "incomming conn" ch)
     (server/send! ch "hello")
     (swap! connections conj ch)
     (server/on-close
@@ -31,36 +45,29 @@
           (println "channel closed: " status)))
     (server/on-receive
      ch (fn [data]
-          (server/send! ch (json/generate-string (dispatch-socket-request (json/parse-string data))))))))
-
-(server/send!
- (first @connections)
- "hello")
+          (server/send! ch
+                        (json/generate-string
+                         (dispatch-socket-request (json/parse-string data keyword))))))))
 
 
-(def fns-map {:index (fn [_] {:body "<html></html>"})
-              :connection #'connection-handler})
 
 (defn format-mw [f]
   (fn [req]
     (let [resp (f req)]
-      (if (:body resp)
-        ;; (update resp :body json/generate-string)
-        (update resp :body yaml/generate-string)
+      (if (and (:body resp) (coll? (:body resp)))
+        (update resp :body json/generate-string)
+        ;;(update resp :body yaml/generate-string)
         resp))))
 
-(defn resolve-route [f]
-  (fn [{uri :uri meth :request-method :as req}]
-    (if-let [m (routing/match [meth uri] #'routes)]
-      (f (assoc req :current-route (:match m) :route-params (:params m)))
-      {:status 404 :body {:message (str uri " is not found")}})))
+(defn *dispatch [{uri :uri meth :request-method :as req}]
+  (if-let [m (routing/match [meth uri] #'routes)]
+    (let [req (assoc req :current-route (:match m) :route-params (:params m))]
+      (if-let [h (get fns-map (:action (:match m)))]
+        (h req)
+        {:status 404 :body {:message (str "No implementation for " (:match m))}}))
+    {:status 404 :body {:message (str uri " is not found")}}))
 
-(defn *dispatch [{route :current-route :as req}]
-  (if-let [h (get fns-map (:action route))]
-    (h req)
-    {:status 404 :body {:message (str "No implementation for " route)}}))
-
-(def dispatch (-> *dispatch resolve-route ))
+(def dispatch (-> *dispatch format-mw))
 ;; format-mw
 
 (defn start []
